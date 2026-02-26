1a267550a94d185688abe7140f66f5bf89b448ff
Aravind Kamath <kamatharavind28@gmail.com>
2026-02-26 23:06:08 +0530

    h2 setup completed

--- src/main/java/com/jpmc/midascore/kafka/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/kafka/TransactionListener.java
@@ -1,13 +1,21 @@
 package com.jpmc.midascore.kafka;
 
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.service.TransactionService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransactionListener {
+
+    private final TransactionService transactionService;
+
+    public TransactionListener(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
     @KafkaListener(topics = "${general.kafka-topic}")
     public void listen(Transaction transaction) {
-        System.out.println("Received transaction: " + transaction);
+        transactionService.processTransaction(transaction);
     }
 }
